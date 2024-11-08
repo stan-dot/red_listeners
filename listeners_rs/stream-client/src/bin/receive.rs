527e4476c8dac6c2939fb63c7bd7562c2d8010c2
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-11-08 17:12:38 +0000

    got the tutorial working

--- listeners_rs/stream-client/src/bin/receive.rs
+++ listeners_rs/stream-client/src/bin/receive.rs
@@ -1,21 +1,22 @@
-use std::io::stdin;
-use rabbitmq_stream_client::error::StreamCreateError;
+use futures::StreamExt;
+use rabbitmq_stream_client::error::{ConsumerCreateError, StreamCreateError};
 use rabbitmq_stream_client::types::{ByteCapacity, OffsetSpecification, ResponseCode};
-use futures::{StreamExt};
+use std::io::stdin;
 use tokio::task;
 
 use rabbitmq_stream_client::Environment;
 
-let environment = Environment::builder().build().await?;
-let stream = "hello-rust-stream";
-let create_response = environment
+async fn receive() -> Result<(), ConsumerCreateError> {
+    let environment = Environment::builder().build().await?;
+    let stream = "hello-rust-stream";
+    let create_response = environment
         .stream_creator()
         .max_length(ByteCapacity::GB(5))
         .create(stream)
         .await;
 
-if let Err(e) = create_response {
-    if let StreamCreateError::Create { stream, status } = e {
+    if let Err(e) = create_response {
+        if let StreamCreateError::Create { stream, status } = e {
             match status {
                 // we can ignore this error because the stream already exists
                 ResponseCode::StreamAlreadyExists => {}
@@ -26,21 +27,36 @@ if let Err(e) = create_response {
         }
     }
 
-
-
-let mut consumer = environment
+    let mut consumer = environment
         .consumer()
         .offset(OffsetSpecification::First)
         .build(stream)
         .await
         .unwrap();
 
-let handle = consumer.handle();
-task::spawn(async move {
+    let handle = consumer.handle();
+    task::spawn(async move {
         while let Some(delivery) = consumer.next().await {
             let d = delivery.unwrap();
-            println!("Got message: {:#?} with offset: {}",
-                     d.message().data().map(|data| String::from_utf8(data.to_vec()).unwrap()),
-                     d.offset(),);
+            println!(
+                "Got message: {:#?} with offset: {}",
+                d.message()
+                    .data()
+                    .map(|data| String::from_utf8(data.to_vec()).unwrap()),
+                d.offset(),
+            );
         }
     });
+    println!("press any key to exit");
+
+    _ = stdin().read_line(&mut String::new());
+    handle.close().await.unwrap();
+    print!("Closed consumer");
+    Ok(())
+}
+
+fn main() {
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    runtime.block_on(receive()).unwrap();
+    let _ = stdin().read_line(&mut String::new());
+}
--- listeners_rs/stream-client/src/bin/send.rs
+++ listeners_rs/stream-client/src/bin/send.rs
@@ -1,29 +1,50 @@
 use rabbitmq_stream_client::error::StreamCreateError;
 use rabbitmq_stream_client::types::{ByteCapacity, Message, ResponseCode};
 
+use rabbitmq_stream_client::{Environment, Producer};
 
-use rabbitmq_stream_client::Environment;
-let environment = Environment::builder().build().await?;
+async fn send() -> Result<(), StreamCreateError> {
+    let environment = Environment::builder().build().await?;
 
-let stream = "hello-rust-stream";
-let create_response = environment
-    .stream_creator()
-    .max_length(ByteCapacity::GB(5))
-    .create(stream)
-    .await;
+    let stream = "hello-rust-stream";
 
-if let Err(e) = create_response {
-    if let StreamCreateError::Create { stream, status } = e {
-        match status {
-            // we can ignore this error because the stream already exists
-            ResponseCode::StreamAlreadyExists => {}
-            err => {
-                println!("Error creating stream: {:?} {:?}", stream, err);
+    let create_response = environment
+        .stream_creator()
+        .max_length(ByteCapacity::GB(5))
+        .create(stream)
+        .await;
+
+    if let Err(e) = create_response {
+        if let StreamCreateError::Create { stream, status } = e {
+            match status {
+                // we can ignore this error because the stream already exists
+                ResponseCode::StreamAlreadyExists => {}
+                err => {
+                    println!("Error creating stream: {:?} {:?}", stream, err);
+                }
             }
         }
     }
+    let producer_result = environment.producer().build(stream).await;
+    let producer = match producer_result {
+        Ok(producer) => producer,
+        Err(e) => {
+            println!("Error creating producer: {:?}", e);
+            return Ok(());
+        }
+    };
+
+    let response = producer
+        .send_with_confirm(Message::builder().body("Hello, World!").build())
+        .await
+        .unwrap();
+
+    println!("Message sent: {:?}", response);
+
+    Ok(())
 }
-let producer = environment.producer().build(stream).await?;
-producer
-    .send_with_confirm(Message::builder().body("Hello, World!").build())
-    .await?;
+
+fn main() {
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    runtime.block_on(send()).unwrap();
+}
